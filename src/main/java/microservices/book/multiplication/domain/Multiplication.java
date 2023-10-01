c4f10a9900b450b4a770f0563103dec6c03e5244
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-10-01 05:28:56 +0200

    Add lombok dependency, add lombok annotations to the Multiplication class

--- src/main/java/microservices/book/multiplication/domain/Multiplication.java
+++ src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -1,32 +1,25 @@
 package microservices.book.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
-* This class represents a Multiplication in our application. @Kedi
+* This represents a Multiplication (a * b). We are now using lombok annotations
 */
-public class Multiplication {
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
 
 	// Both factors
-	private int factorA;
-	private int factorB;
+	private final int factorA;
+	private final int factorB;
 
-	// The result of the operation A * B
-	private int result;
-	
-	public Multiplication(int factorA, int factorB) {
-		this.factorA = factorA;
-		this.factorB = factorB;
-		this.result = factorA * factorB;
-	}
-	public int getFactorA() {
-		return factorA;
-	}
-	public int getFactorB() {
-		return factorB;
-	}
-	public int getResult() {
-		return result;
-	}
-	@Override
-	public String toString() {
-		return "Multiplication{" + "factorA=" + factorA + ", factorB=" + factorB +", result(A*B)=" + result +'}';
+	// Empty constructor for JSON (de)serialization
+	Multiplication() {
+		this(0, 0);
 	}
 }
