ea77b9bb502cd7be9641a773645ac38cc19b4fa4
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-10-01 16:37:03 +0200

    Add multiplication result attempt controller

---
+++ src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -0,0 +1,29 @@
+package microservices.book.multiplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import microservices.book.multiplication.service.MultiplicationService;
+
+@RestController
+@RequestMapping("/results")
+final class MultiplicationResultAttemptController {
+	private final MultiplicationService multiplicationService;
+
+	@Autowired
+	MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
+		this.multiplicationService = multiplicationService;
+	}
+	
+	// Here we'll implement our POST later
+	@RequiredArgsConstructor
+	@NoArgsConstructor(force = true)
+	@Getter
+	public static final class ResultResponse {
+		private final boolean correct;
+	}
+}
