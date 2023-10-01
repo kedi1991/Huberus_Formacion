a157a622794ea7ff3f058c399a1a6dd4920a72fd
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-10-01 03:32:48 +0200

    Using JUnit libs instead of Jupiter for the tests

--- src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
+++ src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
