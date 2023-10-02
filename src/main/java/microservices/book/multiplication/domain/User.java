981f978ee9787109ad44b6c8309235cee1c5649d
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-10-02 03:47:30 +0200

    Add JPA annotations to the User entity

--- src/main/java/microservices/book/multiplication/domain/User.java
+++ src/main/java/microservices/book/multiplication/domain/User.java
@@ -1,5 +1,9 @@
 package microservices.book.multiplication.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +17,13 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "USER_ID")
+	private Long id;
 	private final String alias;
 	
 	// Empty constructor for JSON (de)serialization
