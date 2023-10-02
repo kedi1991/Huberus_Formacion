6b008ce7238ebc7e6c88b1a9b1342a0a268ac67a
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-10-02 03:44:42 +0200

    Modify the Multiplication Entity for JPA operations

--- src/main/java/microservices/book/multiplication/domain/Multiplication.java
+++ src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -1,5 +1,9 @@
 package microservices.book.multiplication.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +16,13 @@ import lombok.ToString;
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class Multiplication {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "MULTIPLICATION_ID")
+	private Long id;
 	// Both factors
 	private final int factorA;
 	private final int factorB;
