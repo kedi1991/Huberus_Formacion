375c39dfb348e9d456fcaa26f18b04c5beccdad6
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-10-02 05:11:47 +0200

    Correct h2 connection properties, edit index html and js to diplay results

--- src/main/resources/static/multiplication-client.js
+++ src/main/resources/static/multiplication-client.js
@@ -9,6 +9,30 @@ $.ajax({url: "http://localhost:8080/multiplications/random"}).then(function(data
 	$('.multiplication-b').empty().append(data.factorB);
 	});
 }
+
+function updateStats(alias) {
+	$.ajax({url: "http://localhost:8080/results?alias=" + alias,}).then(
+			function(data) {
+				$('#stats-body').empty();
+				data.forEach(function(row) {
+								$('#stats-body').append('<tr><td>' + 
+													row.id + 
+													'</td>' + 
+													'<td>' + 
+													row.multiplication.factorA + 
+													' x ' + 
+													row.multiplication.factorB + 
+													'</td>' + 
+													'<td>' + 
+													row.resultAttempt + 
+													'</td>' + 
+													'<td>' + 
+													(row.correct === true ? 'YES' : 'NO') + 
+													'</td></tr>');
+						});	
+			});
+}
+
 $(document).ready(function() {
 	updateMultiplication();
 	$("#attempt-form").submit(function( event ) {
